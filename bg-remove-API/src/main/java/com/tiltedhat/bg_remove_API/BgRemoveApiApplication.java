85354cd0e13d76bcf66497309329cf5e343f50d0
wchandler2020 <vastyle2010@gmail.com>
2025-05-19 09:59:34 -0500

    initial commit

---
+++ bg-remove-API/src/main/java/com/tiltedhat/bg_remove_API/BgRemoveApiApplication.java
@@ -0,0 +1,13 @@
+package com.tiltedhat.bg_remove_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BgRemoveApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BgRemoveApiApplication.class, args);
+	}
+
+}
---
+++ bg-remove-API/src/test/java/com/tiltedhat/bg_remove_API/BgRemoveApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiltedhat.bg_remove_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BgRemoveApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
